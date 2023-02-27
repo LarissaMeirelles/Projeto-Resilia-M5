f970821126fbe4ef9749fe02124b6a84f45df843
Ana Beatriz Castro <113535073+beatrizprog@users.noreply.github.com>
2023-02-27 09:28:19 -0300

    Add files via upload

---
+++ src/controllers/categoryControl.js
@@ -0,0 +1,74 @@
+/**
+ * control/categoryControl.js
+ * Controller da tabela "category" do banco de dados.
+ */
+
+// Importa conector do banco de dados.
+const conn = require('../model/mysql');
+
+// Objeto "controller" para a entidade "category" do banco de dados.
+const categoryControl = {
+
+  // Lista todos os registros válidos.
+  getAll: async (req, res) => {
+    try {
+      const [rows] = await conn.query("SELECT * FROM category;");
+      res.json({ data: rows });
+    } catch (error) {
+      res.json({ status: "error", message: error });
+    }
+  },
+
+  // Lista um registro único pelo Id.
+  getOne: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [rows] = await conn.query("SELECT * FROM category WHERE c_id = ?", [id]);
+      res.json({ data: rows });
+    } catch (error) {
+      res.json({ status: "error", message: error });
+    }
+  },
+
+  // apaga um registro único pelo Id.
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params
+      const sql = "DELETE FROM category WHERE c_id = ?"
+      const [rows] = await conn.query(sql, [id]);
+      res.json({ data: rows });
+    } catch (error) {
+      res.json({ status: "error", message: error });
+    }
+
+  },
+
+  // Insere um novo registro.
+  post: async (req, res) => {
+    try {
+      const { nome, descricao } = req.body;
+      const sql = "INSERT INTO category (c_name, c_description) VALUES (?, ?, ?, ?, ?, ?)";
+      const [rows] = await conn.query(sql, [nome, descricao]);
+      res.json({ data: rows });
+    } catch (error) {
+      res.json({ status: "error", message: error });
+    }
+  },
+
+  // Edita o registro pelo Id.
+  put: async (req, res) => {
+    try {
+      const { nome, descricao } = req.body;
+      const { id } = req.params;
+      const sql = "UPDATE category SET c_name = ?, c_description = ? WHERE c_id = ?"
+      const [rows] = await conn.query(sql, [nome, descricao, id]);
+      res.json({ data: rows });
+    } catch (error) {
+      res.json({ status: "error", message: error });
+    }
+  }
+};
+
+// Exporta o módulo.
+module.exports = categoryControl;
+
