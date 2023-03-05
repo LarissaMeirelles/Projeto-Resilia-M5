6ee3d92f38d728f19ffff333fac1b2fb43fd84e8
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2023-03-05 10:55:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const path = require('path')
 const app = express();
 
 
+
+
+app.use(cors())
 app.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,6 +17,7 @@ app.use((req, res, next)=> {
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
   next()
 })
+
 
 /*
 
